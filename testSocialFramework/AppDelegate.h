8a6825a7027507cfef4607b70590e5f05051713e
snaill <snaill@jeebook.com>
2013-03-07 21:50:32 +0800

    Initial Commit

---
+++ testSocialFramework/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testSocialFramework
+//
+//  Created by Snaill on 13-3-7.
+//  Copyright (c) 2013年 Snail Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testSocialFramework/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testSocialFramework
+//
+//  Created by Snaill on 13-3-7.
+//  Copyright (c) 2013年 Snail Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
